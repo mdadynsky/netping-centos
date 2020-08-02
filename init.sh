e78965fe375092105939d2fe883b1badf9516c75
root <root@localhost.localdomain>
2020-08-02 02:31:45 -0400

    Проверка наличия исходного ISO в локальной папке

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #init
 URL=http://mirror.corbina.net/pub/Linux/centos/8.2.2004/isos/x86_64/CentOS-8.2.2004-x86_64-minimal.iso
-SRC=CentOS-8.2.2004-x86_64-minimal.iso
+
+ISO1=CentOS-Official.iso
+ISO2=Centos-NetPing.iso
 
 #Install soft
 dnf install epel-release -y
@@ -8,4 +10,9 @@ dnf install ntfs-3g -y
 dnf install wget -y
 
 #Download linux
-wget -O Centos-Official.iso $URL
+if [ ! -f "$ISO1" ]; then	
+    echo "$ISO1 does not exists. Dowload..."
+    wget -O $ISO1 $URL
+else
+    echo "$ISO1 exist."
+fi
