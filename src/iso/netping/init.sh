0af744f9a7db7d1a6e8864042f4813ff6d7582b8
root <root@localhost.localdomain>
2020-08-04 06:31:55 -0400

    rpm не законченно

--- src/iso/netping/init.sh
+++ src/iso/netping/init.sh
@@ -22,4 +22,10 @@ status)
 esac
 	
 echo "init Ok Centos 8" > /root/ok.txt
+
+cd /netping/rpm
+
+rpm -ihv  mariadb-server-10.3.17-1.module_el8.1.0+257+48736ea6.x86_64.rpm  
+rpm -ihv  zabbix-apache-conf-5.0.2-1.el8.noarch.rpm zabbix-server-mysql-5.0.2-1.el8.x86_64.rpm zabbix-agent-5.0.2-1.el8.x86_64.rpm zabbix-release-5.0-1.el8.noarch.rpm zabbix-web-mysql-5.0.2-1.el8.noarch.rpm
+
 exit 0 
